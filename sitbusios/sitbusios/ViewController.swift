e480f4f864f1c592f85a0df346405620d95c2f61
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-04-06 11:27:10 +0200

    Initialize project

---
+++ sitbusios/sitbusios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sitbusios
+//
+//  Created by lpcm on 06/04/2016.
+//  Copyright © 2016 lpcm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
