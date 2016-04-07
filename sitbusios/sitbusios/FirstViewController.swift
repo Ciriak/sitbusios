e0822379a436cb289326317fd64e422962ff9fcf
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-04-07 12:30:59 +0200

    Swift < Hitler < Cancer

--- sitbusios/sitbusios/FirstViewController.swift
+++ sitbusios/sitbusios/FirstViewController.swift
@@ -7,13 +7,20 @@
 //
 
 import UIKit
+//import Alamofire  <-Pas moyen, même avec un Podfile il veut pas
+
 
 var token = "5beafb4a-b02f-429c-8222-cd92d1c20355";
-var baseUrl = "https://"+token+"@api.navitia.io/v1/coverage/fr-idf/networks";
+var baseUrl = "https://"+token+"@api.navitia.io/v1/coverage/fr-idf/";
 
 class FirstViewController: UIViewController {
     
     var loc = CoreLocationController();
+    
+    // Coup de gueule :
+    /* C'est quoi ce bordel pour récupérer un pauvre JSON ??? Sur mobile c'est genre le truc DE BASE dire qu'en web on peut faire $.getJSON('xx');, tsss c'est vraiment pourrie SWIFT !!!*/
+    
+    // Pas moyen de
     
 
     @IBOutlet weak var stopTimeRemaining: UILabel!
@@ -21,6 +28,28 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var stopDetailsLabel: UILabel!
     let stopDetailsBase = "Avant le prochains passage à ";
     override func viewDidLoad() {
+        
+        
+        let requestURL: NSURL = NSURL(string: baseUrl+"coords/2.637192;48.804668/stop_areas?distance=500")!
+        let urlRequest: NSMutableURLRequest = NSMutableURLRequest(URL: requestURL)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(urlRequest) {
+            (data, response, error) -> Void in
+            
+            let httpResponse = response as! NSHTTPURLResponse
+            let statusCode = httpResponse.statusCode
+            
+            if (statusCode == 200) {
+                print("Everyone is fine, file downloaded successfully.")
+            }
+            else {
+                print("HTTP error "+String(statusCode)+" "+String(httpResponse));
+            }
+
+        }
+        
+        task.resume();
+    
         print(baseUrl);
         super.viewDidLoad()
         stopTimeRemaining.text = "8 minutes";
